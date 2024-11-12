f9da043a55962949c6d651b24160ef591671c3f1
JkinkJS <jkinkjs24@outlook.com>
2024-11-12 17:56:04 +0100

    12 Nov - 24 - 17:54
    Verificamos la estrucutra de jason en tree
    ahora llamamos la ficha de superheroe de toda la estructura de Jason  en SuperheroeService
      @GET ("{character-id}")
        suspend fun findSuperheroesById(@Path("character-id") id: String) : fichaSuperHeroe

--- app/src/main/java/com/example/superheroe/services/SuperheroService.kt
+++ app/src/main/java/com/example/superheroe/services/SuperheroService.kt
@@ -2,6 +2,7 @@ package com.example.superheroe.services
 
 import com.example.superheroe.data.Superhero
 import com.example.superheroe.data.SuperheroResponse
+import com.example.superheroe.data.fichaSuperHeroe
 import retrofit2.http.GET
 import retrofit2.http.Path
 
@@ -10,9 +11,10 @@ interface SuperheroService {
     @GET ("search/{name}")
     suspend fun findSuperheroesByName(@Path("name") query: String) : SuperheroResponse
 
+   // @GET ("{character-id}")
+   // suspend fun findSuperheroesById(@Path("character-id") id: String) : Superhero
+
+
     @GET ("{character-id}")
-    suspend fun findSuperheroesById(@Path("character-id") id: String) : Superhero
-
-
-
+    suspend fun findSuperheroesById(@Path("character-id") id: String) : fichaSuperHeroe
 }
